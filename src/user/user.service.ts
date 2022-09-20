19a43697e44f51eccd3eacb3040536a40c5cf250
valdir-alves3000 <valdiralves3000@gmail.com>
2022-09-20 18:47:58 -0300

    Create user with encrypted password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const passwordHash = await hash(createUserDto.password, 10);
 
-  findAll() {
-    return `This action returns all user`;
-  }
+    const data = {
+      ...createUserDto,
+      password: passwordHash,
+    };
 
+    const createUser = await this.prisma.user.create({ data });
+
+    return {
+      ...createUser,
+      password: undefined,
+    };
+  }
   findOne(id: number) {
     return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
