5982e8186decf623e4822ed9c40c80e66bb8826d
valdir-alves3000 <valdiralves3000@gmail.com>
2022-09-20 20:45:48 -0300

    Auth: implement login generate JWT

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
@@ -9,10 +9,5 @@ export class UserController {
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
-  }
-
-  @Get(':email')
-  async findByEmail(@Param('email') email: string) {
-    return this.userService.findByEmail(email);
   }
 }
